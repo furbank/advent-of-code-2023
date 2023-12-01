ef664800c5dcdc1c88f182883d0f85e4216179bd
Stephen Furbank <furbank@hotmail.com>
2023-12-01 18:40:45 +0000

    day-01 its never that simple

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -8,7 +8,9 @@ fn part2(input: &str) -> u32 {
     let mut total: u32 = 0;
 
     let lines: Vec<String> = input.lines().map(String::from).collect();
-    for l in &lines {
+    for line in &lines {
+        let l = replace_numbers(line);
+        println!("{} --> {}", line, l);
         let d: Vec<char> = l.chars().filter(|c| c.is_digit(10)).collect();
         let num: String = format!("{}{}", d[0], d[d.len() - 1]);
         let int: u32 = num.parse().unwrap();
@@ -16,6 +18,21 @@ fn part2(input: &str) -> u32 {
     }
     total
 }
+
+fn replace_numbers(line: &String) -> String {
+    let mut newline = line.replace("one", "1");
+    newline = newline.replace("two", "2");
+    newline = newline.replace("three", "3");
+    newline = newline.replace("four", "4");
+    newline = newline.replace("five", "5");
+    newline = newline.replace("six", "6");
+    newline = newline.replace("seven", "7");
+    newline = newline.replace("eight", "8");
+    newline = newline.replace("nine", "9");
+
+    newline.to_string()
+}
+
 
 #[cfg(test)]
 mod tests {
