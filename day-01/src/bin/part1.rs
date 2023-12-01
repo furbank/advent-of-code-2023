41ca32c9eb40413b459d7ed5206b3f483b36f339
Stephen Furbank <furbank@hotmail.com>
2023-12-01 13:50:48 +0000

    empty day-01

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,14 +1,3 @@
-# Advent of Code 2023
-Learning some rust by struggling through problems.
-
-## Handy Links
-[Advent of code 2023](https://adventofcode.com/2023)
-[How to set up Rust for Advent of Code](https://www.youtube.com/watch?v=fEQv-cqzbPg)
-[GitIgnore for Rust](https://github.com/rust-lang/cargo/blob/master/.gitignore)
-
-## Starting Boilerplate
-
-```rust
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part1(input);
@@ -30,4 +19,3 @@ mod tests {
         assert_eq!(result, "4".to_string());
     }
 }
-```
