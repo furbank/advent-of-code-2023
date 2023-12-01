0d0bf027f98b9226ff360f3b3de2f3bd88c79ead
Stephen Furbank <furbank@hotmail.com>
2023-12-01 17:37:15 +0000

    day-01 setup part2 with failing test

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Part 2");
+    let input = include_str!("./input1.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+fn part2(input: &str) -> u32 {
+    let mut total: u32 = 0;
+
+    let lines: Vec<String> = input.lines().map(String::from).collect();
+    for l in &lines {
+        let d: Vec<char> = l.chars().filter(|c| c.is_digit(10)).collect();
+        let num: String = format!("{}{}", d[0], d[d.len() - 1]);
+        let int: u32 = num.parse().unwrap();
+        total += int
+    }
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    //use crate::part2;
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = part2(
+            "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen",
+        );
+
+        assert_eq!(result, 281);
+    }
 }
