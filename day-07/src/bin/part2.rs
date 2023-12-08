525d1ef6893dc7cff26ee300376e12004627deb0
Stephen Furbank <furbank@hotmail.com>
2023-12-08 13:17:58 +0000

    day-07 part 2 completed

--- day-07/src/bin/part2.rs
+++ day-07/src/bin/part2.rs
@@ -24,10 +24,10 @@ fn part2(input: &str) -> String {
         .map(|(i, h)| ((i + 1) * h.bid))
         .sum::<usize>();
 
-        total.to_string()
+    total.to_string()
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum HandType {
     FiveOfaKind,
     FourOfaKind,
@@ -37,7 +37,7 @@ enum HandType {
     OnePair,
     HighCard,
 }
-// #[derive(Debug,Eq, Ord, PartialEq, PartialOrd)]
+
 struct Hand {
     cards: String,
     bid: usize,
@@ -50,8 +50,111 @@ impl Hand {
         Self {
             cards: split_line.0.to_string(),
             bid: split_line.1.parse::<usize>().unwrap(),
-            hand_type: Self::find_type(split_line.0),
+            hand_type: Self::best_hand(split_line.0),
         }
+    }
+
+    fn best_hand(cards: &str) -> HandType {
+        let jokers = cards.matches("J").count();
+
+        if jokers == 0 {
+            return Self::find_type(cards);
+        }
+
+        if jokers == 5 || jokers == 4 {
+            return HandType::FiveOfaKind;
+        }
+
+        // Promote the remaining joker hands
+        if jokers == 3 {
+            // print!(
+            //     "{} cards: {} - type {:?}",
+            //     jokers,
+            //     cards,
+            //     Self::find_type(cards)
+            // );
+
+            match Self::find_type(cards) {
+                HandType::ThreeOfaKind => {
+                    // println!(" >> {:?}", HandType::FourOfaKind);
+                    return HandType::FourOfaKind;
+                }
+                HandType::FullHouse => {
+                    // println!(" >> {:?}", HandType::FiveOfaKind);
+                    return HandType::FiveOfaKind;
+                }
+                _ => (),
+            }
+
+            // println!("panic!");
+            panic!("Problem in the 3 jokers");
+        }
+
+        if jokers == 2 {
+            // print!(
+            //     "{} cards: {} - type {:?}",
+            //     jokers,
+            //     cards,
+            //     Self::find_type(cards)
+            // );
+
+            match Self::find_type(cards) {
+                HandType::FullHouse => {
+                    // println!(" >> {:?}", HandType::FiveOfaKind);
+                    return HandType::FiveOfaKind;
+                }
+                HandType::TwoPair => {
+                    // println!(" >> {:?}", HandType::FourOfaKind);
+                    return HandType::FourOfaKind;
+                }
+                HandType::OnePair => {
+                    // println!(" >> {:?}", HandType::ThreeOfaKind);
+                    return HandType::ThreeOfaKind;
+                }
+                _ => (),
+            }
+
+            // println!("panic!");
+            panic!("Problem with 2 jokers!")
+        }
+
+        if jokers == 1 {
+            // print!(
+            //     "{} cards: {} - type {:?}",
+            //     jokers,
+            //     cards,
+            //     Self::find_type(cards)
+            // );
+
+            match Self::find_type(cards) {
+                HandType::FourOfaKind => {
+                    // println!(" >> {:?}", HandType::FiveOfaKind);
+                    return HandType::FiveOfaKind;
+                }
+                HandType::ThreeOfaKind => {
+                    // println!(" >> {:?}", HandType::FourOfaKind);
+                    return HandType::FourOfaKind;
+                }
+                HandType::TwoPair => {
+                    // println!(" >> {:?}", HandType::FullHouse);
+                    return HandType::FullHouse;
+                }
+                HandType::OnePair => {
+                    // println!(" >> {:?}", HandType::ThreeOfaKind);
+                    return HandType::ThreeOfaKind;
+                }
+                HandType::HighCard => {
+                    // println!(" >> {:?}", HandType::OnePair);
+                    return HandType::OnePair;
+                }
+                _ => (),
+            }
+
+            // println!("panic!");
+            panic!("Problem with 1 joker!")
+        }
+
+        return return HandType::HighCard;
     }
 
     fn find_type(cards: &str) -> HandType {
@@ -110,16 +213,16 @@ impl Hand {
             'A' => "13".to_string(),
             'K' => "12".to_string(),
             'Q' => "11".to_string(),
-            'J' => "10".to_string(),
-            'T' => "09".to_string(),
-            '9' => "08".to_string(),
-            '8' => "07".to_string(),
-            '7' => "06".to_string(),
-            '6' => "05".to_string(),
-            '5' => "04".to_string(),
-            '4' => "03".to_string(),
-            '3' => "02".to_string(),
-            '2' => "01".to_string(),
+            'T' => "10".to_string(),
+            '9' => "09".to_string(),
+            '8' => "08".to_string(),
+            '7' => "07".to_string(),
+            '6' => "06".to_string(),
+            '5' => "05".to_string(),
+            '4' => "04".to_string(),
+            '3' => "03".to_string(),
+            '2' => "02".to_string(),
+            'J' => "01".to_string(),
             _ => "".to_string(),
         }
     }
